7131f8c150e0d15b302cb614934ddfedf713c22a
Olga Saxina <olga.saksina@gmail.com>
2020-03-07 14:31:38 +0300

    Lab1

---
+++ Lab1/src/main/java/telematics/labs/Main.java
@@ -0,0 +1,34 @@
+package telematics.labs;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        String from =  in.nextLine();
+        String to =  in.nextLine();
+        copyFile(from, to);
+        in.close();
+    }
+    public static void copyFile( String from, String to ) throws IOException {
+
+        File src = new File(from);
+        File dest = new File(to);
+
+        if(Files.exists(src.toPath())){
+            if(!Files.exists(dest.toPath())) {
+                Files.copy(src.toPath(), dest.toPath());
+            }
+            else {
+                System.out.println("File already exists");
+            }
+        }
+        else{
+            System.out.println("File is not found");
+        }
+    }
+}
---
+++ Lab1/src/test/java/telematics/labs/Tests.java
@@ -0,0 +1,17 @@
+package telematics.labs;
+
+import java.nio.file.Files;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class Tests {
+
+    @Test
+
+	public void test() {
+        copyFile();
+	}
+
+}
